0e608ae96247a79bc593d019ddc5e9af43a83ad9
tochapado <tochapado710@gmail.com>
2022-06-30 21:36:00 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+require("dotenv").config()
+
+const express = require("express");
+const app = express();
+
+const multer = require("multer");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const File = require("./models/File.js")
+
+const upload = multer({ dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URL)
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
+
+  const file = await File.create(fileData);
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+});
+
+app.get("/file/:id", async (req, res) => {
+  const file = await File.findById(req.params.id);
+
+  file.downloadCount++;
+  await file.save();
+
+  res.download(file.path, file.originalName);
+})
+
+app.listen(process.env.PORT);
